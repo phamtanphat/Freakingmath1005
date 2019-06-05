816ebfc912a29100b73e18dae4a29345d110ef1a
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-05 14:25:32 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/freakingmath1005/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/freakingmath1005/MainActivity.java
@@ -9,5 +9,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //1 : Random
+//            + Random số thứ 1 và 2 < 10
+//            + Random phep tinh (+ - * /)
+        //2 : Xu ly giao dien
+//            +Gắn giá trị số thứ 1 và thứ 2 vào giao diện
+//            +Gắn kết quả sau khi thực thi phép tính
+//            +Gắn phép tính vào
+        //3 : Xử lý cho bài toán sai
+//            + Random 1 biến true false để xử lý cho kết quả sai
+        // 4: Bắt sự kiện 2 imageviewButton
+//            +Nếu biến check = true va kết là đúng thì ok có điểm
+//            +Nếu check = false va kết quả là sai thì có điểm
     }
 }
